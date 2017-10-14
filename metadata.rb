0eb9ae0069fc1a91e6213f5b5ab99a1146dd1b4b
Kishan B <kishancs46@gmail.com>
2017-10-14 21:35:38 +0530

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "chef-ruby-shadow"
 maintainer       "Tom Howe"
-maintainer_email "turtlebender at gmail.com"
+maintainer_email "turtlebender@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures ruby-shadow"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
